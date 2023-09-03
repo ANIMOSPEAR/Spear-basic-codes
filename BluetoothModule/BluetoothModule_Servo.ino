c0d5a345c8b7060849308e41b8bdaa5e9faded2d
ANIMOSPEAR <143713210+ANIMOSPEAR@users.noreply.github.com>
2023-09-03 22:47:22 +0800

    Add files via upload

---
+++ BluetoothModule/BluetoothModule_Servo.ino
@@ -0,0 +1,36 @@
+//code by Paul Khristian B. Marasigan
+
+#include <Servo.h>          
+#define UP 180              
+#define DOWN 0             
+
+Servo myservo;
+
+char bluetooth;
+int myservo_pin = 4;
+
+void myservo_pos(int pos) {
+  myservo.write(pos);
+}
+
+void setup() {
+ myservo.attach(myservo_pin);
+ Serial.begin(9600);       
+ 
+}
+
+void loop() {
+if(Serial.available()){    
+  bluetooth = Serial.read();
+  Serial.println(bluetooth);
+  }
+  if(bluetooth == 'F'){            
+  myservo_pos(UP);
+  delay(1000);
+  myservo_pos(DOWN);
+  delay(1000);
+ }
+  if(bluetooth == 'B'){            
+  myservo_pos(DOWN);
+ }
+}
